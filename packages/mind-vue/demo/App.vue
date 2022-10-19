9b234a258bf45c6dae0573508b3bb2333553cb89
x <hhxu@jihulab.com>
2022-10-19 10:32:53 +0800

    feat: add vue version dir

---
+++ packages/mind-vue/demo/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <tf-button />
+</template>
+
+<script lang="ts" setup>
+import { TfButton } from "../lib";
+</script>
+
+<style lang="scss"></style>
---
+++ packages/mind-vue/rollup.config.js
@@ -0,0 +1,50 @@
+import uglify from '@lopatnov/rollup-plugin-uglify'
+import commonjs from '@rollup/plugin-commonjs'
+import postcss from 'rollup-plugin-postcss'
+import typescript from 'rollup-plugin-typescript2'
+import vue from "rollup-plugin-vue";
+import pkg from './package.json'
+
+export default {
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  input: './index.ts',
+  output: [
+    {
+      file: 'lib/index.js',
+      format: 'cjs',
+      strict: false
+    },
+    {
+      file: 'lib/index.module.js',
+      format: 'es',
+      strict: false
+    },
+    {
+      file: 'lib/index.min.js',
+      format: 'iife',
+      // sourcemap: !production,
+      name: 'mindReact',
+      strict: false
+    },
+    {
+      file: 'lib/index.umd.js',
+      format: 'umd',
+      name: 'mindReact',
+      strict: false
+    }
+  ],
+  plugins: [
+    typescript({
+      useTsconfigDeclarationDir: true
+    }),
+    vue({
+      preprocessStyles: true,
+    }),
+    postcss({
+      modules: true,
+      use: ['sass']
+    }),
+    commonjs(),
+    process.argv.includes('-cw') ? undefined : uglify()
+  ]
+}
